903e83597d2ee5c6bfc6661afe3a0ae01d9733a8
Maxim Ivannikov <84341083+MaximIVannikov@users.noreply.github.com>
2021-11-10 23:21:55 +0100

    added filte and search-panel

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,29 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label: 'Všichni zaměstnanci' },
+        {name: 'rise', label: 'Zvýšení mzdy' },
+        {name: 'moreThan1000', label: 'Mzda výšší než 1000$' }
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return(
+            <button 
+                className={`btn ${clazz}`}
+                type="button"
+                key={name}
+                onClick={() => props.onFilterSelect(name)}>
+                    {label}
+            </button>
+        )
+    })
+
     return(
         <div className="btn-group">
-            <button 
-                className="btn btn-light"
-                type="button">
-                    Všichni zaměstnanci
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Zvýšení mzdy
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Mzda výšší než 1000$
-            </button>
+            {buttons}
         </div>
     );
 }
